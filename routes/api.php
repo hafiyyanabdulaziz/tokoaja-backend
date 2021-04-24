febcae80e7aaead2dbd993f60e60e689bd407243
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-04-24 18:15:27 +0700

    simple post and get

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', function () {
+    return response()->json(
+        [
+            "message" => "GET Sukses"
+        ]
+    );
+});
+
+Route::post('/products', function () {
+    return response()->json(
+        [
+            "message" => "POST Sukses"
+        ]
+    );
+});
